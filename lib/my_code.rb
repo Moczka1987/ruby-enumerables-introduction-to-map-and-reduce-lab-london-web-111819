79daa12339daf9e5428208a0c02313a4d58568a9
Mariola Pasko <paskomariola@gmail.com>
2019-11-04 00:50:06 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -18,6 +18,10 @@ def reduce_to_total(source_array, starting_point)
 source_array.reduce(starting_point, :+)
 end 
 
+def reduce_to_any_true(source_array) 
+source_array.reduce {|el| el = true}
+end 
+
 def reduce_to_all_true(source_array)
 source_array.reduce {|el| el == true}
 end

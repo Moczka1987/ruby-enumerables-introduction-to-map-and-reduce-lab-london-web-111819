1749a13c089c8557619cc4a58049c2165356970b
Mariola Pasko <paskomariola@gmail.com>
2019-11-04 00:56:16 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -15,7 +15,7 @@ source_array.map {|x| x * x }
 end
 
 def reduce_to_total(source_array, starting_point)
-source_array.reduce(starting_point, :+)
+source_array.reduce(starting_point) {|sum, num| 
 end 
 
 def reduce_to_all_true(source_array)
